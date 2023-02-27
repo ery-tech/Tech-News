9dfbea85fe8463b372d08bc819513951c38547b9
Erika <erikaguardo99@gmail.com>
2023-02-27 22:31:57 +0100

    webpack config

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -44,9 +44,7 @@ const config = {
     ]
   },
   plugins: [
-    new CopyPlugin({
-      patterns: [{ from: 'src/index.html' }],
-    }),
+    
     new HtmlWebpackPlugin({
       templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
       filename: 'bundle.html',
